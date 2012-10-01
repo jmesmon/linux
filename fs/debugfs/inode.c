6b26c9fd79b2388de04ead2461db41fb165ad1a0
Cody P Schafer <cody@linux.vnet.ibm.com>
2012-10-01 12:11:44 -0700

    debugfs: dcache paranoia: dget() before d_initialize()
    d_initialize() indicates that it expects a reference to the dentry to be
    held prior to it [d_initialize()] being called.
    
    It doesn't appear debugfs is obeying this. No idea what the potential
    consequences are (if any).

--- fs/debugfs/inode.c
+++ fs/debugfs/inode.c
@@ -83,8 +83,8 @@ static int debugfs_mknod(struct inode *dir, struct dentry *dentry,
 
 	inode = debugfs_get_inode(dir->i_sb, mode, dev, data, fops);
 	if (inode) {
-		d_instantiate(dentry, inode);
 		dget(dentry);
+		d_instantiate(dentry, inode);
 		error = 0;
 	}
 	return error;
