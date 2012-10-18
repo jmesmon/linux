2e24857824d29d32ca7739aa1bc704a0d34f6a06
Cody P Schafer <cody@linux.vnet.ibm.com>
2012-10-18 16:08:40 -0700

    dnuma/memlayout: add memlayout & dnuma to track & transplant pages
    On certain systems, the hypervisor can (and will) relocate physical
    addresses as seen in a VM between real NUMA nodes. For example, IBM's
    Power systems which are using PHYP (their proprietary hypervisor).
    
    This change set introduces the infrastructure for tracking & dynamically
    changing "memory layouts" (or "memlayouts"): the mapping between page
    ranges & the actual backing NUMA node.

---
+++ include/linux/memlayout.h
@@ -0,0 +1,149 @@
+#ifndef LINUX_MEMLAYOUT_H_
+#define LINUX_MEMLAYOUT_H_
+
+#include <linux/memblock.h> /* __init_memblock */
+#include <linux/mm.h>       /* NODE_DATA, page_zonenum */
+#include <linux/mmzone.h>   /* pfn_to_nid */
+#include <linux/rbtree.h>
+#include <linux/types.h>    /* size_t */
+
+#ifdef CONFIG_DYNAMIC_NUMA
+# ifdef NODE_NOT_IN_PAGE_FLAGS
+#  error "CONFIG_DYNAMIC_NUMA requires the NODE is in page flags. Try freeing up some flags by decreasing the maximum number of NUMA nodes, or switch to sparsmem-vmemmap"
+# endif
+
+#if 0
+/* must keep 'range_ct' from memlayout */
+struct pfn_iterator {
+	atomic_t ref_ct;
+	struct flex_array *ranges;
+};
+#endif
+
+enum memlayout_type {
+	ML_INITIAL,
+	ML_DNUMA,
+	ML_NUM_TYPES
+};
+
+/*
+ * - rbtree of {node, start, end}.
+ * - assumes no 'ranges' overlap.
+ */
+struct rangemap_entry {
+	struct rb_node node;
+	unsigned long pfn_start;
+	/* @pfn_end: inclusive, not stored as a count to make the lookup
+	 *           faster
+	 */
+	unsigned long pfn_end;
+	int nid;
+};
+
+/* XXX: add nodemask for use in dnuma_online_required_nodes & build in
+ * memlayout_new_range()? */
+struct memlayout {
+	struct rb_root root;
+	enum memlayout_type type;
+	struct rangemap_entry *cache;
+#if 0
+	unsigned long range_ct;
+	struct pfn_iterator *pfn_iterator;
+#endif
+#ifdef CONFIG_DNUMA_DEBUGFS
+	unsigned seq;
+	struct dentry *d;
+#endif
+};
+
+extern __rcu struct memlayout *pfn_to_node_map;
+
+
+/* FIXME: overflow potential in completion check */
+#define ml_for_each_pfn_in_range(rme, pfn)	\
+	for (pfn = rme->pfn_start;		\
+	     pfn <= rme->pfn_end;		\
+	     pfn++)
+
+#define ml_for_each_range(ml, rme) \
+	for (rme = rb_entry(rb_first(&ml->root), typeof(*rme), node);	\
+	     &rme->node;						\
+	     rme = rb_entry(rb_next(&rme->node), typeof(*rme), node))
+
+struct memlayout *memlayout_create(enum memlayout_type);
+void              memlayout_destroy(struct memlayout *ml);
+
+/* Callers accesing the same memlayout are assumed to be serialized */
+int memlayout_new_range(struct memlayout *ml,
+		unsigned long pfn_start, unsigned long pfn_end, int nid);
+
+/* only queries the memlayout tracking structures. */
+int memlayout_pfn_to_nid_no_pageflags(unsigned long pfn);
+
+/* Put ranges added by memlayout_new_range() into use by
+ * memlayout_pfn_get_nid() and retire old ranges.
+ * Any subsequent uses of memlayout_new_range() begin to build a new range map.
+ *
+ * sleeps via syncronize_rcu().
+ *
+ * memlayout takes ownership of ml, no futher mamlayout_new_range's should be
+ * issued
+ */
+void memlayout_commit(struct memlayout *ml);
+
+/* only commits the changes if it is the first to do so. Otherwise makes no
+ * changes to the memlayout.
+ * For use in arch_memlayout_init() */
+void memlayout_commit_initial(struct memlayout *ml);
+
+/* reqires: slab_is_available()
+ * sleeps: definitely: memlayout_commit() -> synchronize_rcu()
+ *	   potentially: kmalloc()
+ */
+int memlayout_init_from_memblock(void) __init_memblock;
+
+/* defined in an arch code. Called in init/main.c following kmem_cache
+ * initialization. */
+void arch_memlayout_init(void);
+
+#ifdef CONFIG_HAVE_ARCH_MEMLAYOUT_INIT
+#define memlayout_global_init() arch_memlayout_init()
+#else
+#define memlayout_global_init() memlayout_init_from_memblock()
+#endif
+
+#else /* ! defined(CONFIG_DYNAMIC_NUMA) */
+
+/* memlayout_new_range() & memlayout_commit*() are purposefully omitted */
+static inline void memlayout_global_init(void)
+{}
+static inline int memlayout_pfn_to_nid_no_pageflags(unsigned long pfn)
+{ return NUMA_NO_NODE; }
+static inline void arch_memlayout_init(void)
+{}
+static inline int __init_memblock memlayout_init_from_memblock(void)
+{ return 0; }
+#endif
+
+/* falls back on pfn_to_nid() [using pageflags] when memlayout tracking doesn't
+ * know what node the pfn belongs to. */
+static inline int memlayout_pfn_to_nid(unsigned long pfn)
+{
+	int nid = memlayout_pfn_to_nid_no_pageflags(pfn);
+	if (nid == NUMA_NO_NODE)
+		nid = pfn_to_nid(pfn);
+	return nid;
+}
+
+static inline int memlayout_page_to_nid(struct page *page)
+{
+	return memlayout_pfn_to_nid(page_to_pfn(page));
+}
+
+static inline struct zone *memlayout_page_zone(struct page *page)
+{
+	return &NODE_DATA(memlayout_page_to_nid(page))->node_zones[page_zonenum(page)];
+}
+
+
+#endif
---
+++ mm/dnuma.c
@@ -0,0 +1,203 @@
+#define pr_fmt(fmt) "dnuma: " fmt
+
+#include <linux/dnuma.h>
+#include <linux/mm.h>
+#include <linux/mmzone.h>
+#include <linux/spinlock.h>
+#include <linux/types.h>
+#include <linux/atomic.h>
+
+#include "internal.h"
+
+#if CONFIG_DNUMA_DEBUGFS
+atomic64_t dnuma_moved_page_ct;
+#endif
+
+/* Options wrt removing free pages from free lists:
+ * - Isolation? - NO.
+ *   - Can't determine which page ranges to isolate (memory belonging to this
+ *     zone could be anywhwre)
+ *   - too many things being isolated could trigger OOM.
+ *   - Not too terrible if some stuff is allocated.
+ * - lock zone & then grab a "bunch", lock new zone & give it a "bunch".
+ * - Selecting pages to move
+ *   - iterate over pfns
+ *     - with dnuma, zones grow very large, could result in iterating over all
+ *       pfns on the system.
+ *     - pfns we examine could belong to a different zone, requiring skipping.
+ *     - Fits with "destination" focus
+ *   - iterate over free lists
+ *     - pointer dereferences are not as fast as addition (pfn ++, etc).
+ *     - all pages belong to the current zone, no skipping.
+ *     - Fits with "source" focus
+ * - Focus on the source zone or the destination zone (complexities are per
+ *   zone operation, so don't really represent actual cost)
+ *   - source
+ *     - grab all pages in 1 source zone that need moving to any number of
+ *       other zones.
+ *     - repeat for each source zone. TC=O(n), MC=O(n).
+ *   - destination
+ *     - grab all pages from various sources which will be moved to 1
+ *       destination.
+ *     - repeat for each destination zone. TC=O(n), MC=O(1).
+ *     - Potentially very high amount of locking/unlocking.
+ *   - mixed
+ *     - grab all pages in 1 source which need moving to 1 other zone.
+ *     - repeat for all pairs of zones. TC = O(n^n), MC = O(1).
+ *     - High time complexity. Does this actually represent a real slowdown?
+ */
+
+/* Issues due to pageflag_blocks attached to zones with Discontig Mem (&
+ * Flatmem??).
+ * - Need atomicity over the combination of commiting a new memlayout and
+ *   removing the pages from free lists.
+ */
+
+void adjust_zone_present_pages(struct zone *zone, long delta)
+{
+	unsigned long flags;
+	pgdat_resize_lock(zone->zone_pgdat, &flags);
+	zone_span_writelock(zone);
+
+	zone->present_pages += delta;
+	zone->zone_pgdat->node_present_pages += delta;
+
+	zone_span_writeunlock(zone);
+	pgdat_resize_unlock(zone->zone_pgdat, &flags);
+}
+
+/* - must be called under lock_memory_hotplug() */
+void dnuma_online_required_nodes(struct memlayout *new_ml)
+{
+	int nid;
+	struct rangemap_entry *rme;
+	ml_for_each_range(new_ml, rme) {
+		nid = rme->nid;
+		if (!node_online(nid)) {
+			pr_debug("onlining node %d\n", nid);
+			mem_online_node(nid);
+		}
+	}
+}
+
+/* Always called after dnuma_move_to_new_ml() & the assigning of the new
+ * memlayout */
+/* FIXME: folding this into dnuma_move_to_new_ml() could allow us to avoid
+ * re-processing the free pages which are currently transplanted within
+ * dnuma_move_to_new_ml(). */
+void dnuma_mark_page_range(struct memlayout *new_ml)
+{
+	struct rangemap_entry *rme;
+	ml_for_each_range(new_ml, rme) {
+		unsigned long pfn;
+		for (pfn = rme->pfn_start; pfn <= rme->pfn_end; pfn++) {
+			struct page *page = pfn_to_page(pfn);
+			/* FIXME: should we be skipping compound / buddied pages? */
+			SetPageLookupNode(page);
+		}
+	}
+}
+
+/* Does memory allocation in ensure_zone_is_initialized(), must be called with
+ * is_atomic=false (ie: no spinlocks, no rcu).
+ */
+void dnuma_move_to_new_ml(struct memlayout *new_ml)
+{
+	/* XXX: locking considerations:
+	 *  - what can cause the hotplugging of a node? Do we just need to
+	 *    lock_memory_hotplug()?
+	 */
+
+	/* migrate types?
+	 * ISOLATION?
+	 */
+	struct rangemap_entry *rme;
+	ml_for_each_range(new_ml, rme) {
+		int range_nid = rme->nid;
+		unsigned long pfn;
+		struct page *page;
+
+		for (pfn = rme->pfn_start; pfn <= rme->pfn_end; pfn++) {
+			struct zone *zone, *dest_zone;
+			int page_nid, order;
+			unsigned long flags, last_pfn, first_pfn;
+			if (!pfn_valid(pfn))
+				continue;
+
+			page = pfn_to_page(pfn);
+
+			/* Currently allocated, will be fixed up when freed. */
+			if (!PageBuddy(page))
+				continue;
+
+			page_nid = page_to_nid(page);
+			/* Page is already in the "right" zone */
+			if (page_nid == range_nid)
+				continue;
+
+			zone = page_zone(page);
+retry_page:
+			spin_lock_irqsave(&zone->lock, flags);
+
+			/* Someone allocated it since we last checked. It will
+			 * be fixed up when it is freed */
+			if (!PageBuddy(page))
+				goto skip_unlock;
+
+			/* It has already migrated "somewhere" */
+			if (page_zone(page) != zone)
+				goto skip_unlock;
+
+			/* TODO: online required zones outside of atomic (they need memory allocations) */
+			dest_zone = nid_zone(range_nid, page_zonenum(page));
+			if (!zone_is_initialized(dest_zone)) {
+				/* TODO: transplanting this page. */
+				spin_unlock_irqrestore(&zone->lock, flags);
+				/* XXX: locking on this? lock_memory_hotplug()? lock(dest_zone->lock)? */
+				ensure_zone_is_initialized(dest_zone, 0, 0);
+				goto retry_page;
+			}
+
+			/* FIXME: split pages when the range does not cover an
+			 * entire order */
+
+			order = page_order(page);
+			first_pfn = pfn & ~((1 << order) - 1);
+			last_pfn  = pfn |  ((1 << order) - 1);
+			if (WARN(pfn != first_pfn, "pfn %lu is not first_pfn %lu\n",
+							pfn, first_pfn)) {
+				pfn = last_pfn;
+				goto skip_unlock;
+			}
+
+			if (WARN(last_pfn > rme->pfn_end,
+					"last_pfn %lu goes beyond end of rme [ %lu - %lu ]\n",
+					last_pfn, rme->pfn_start, rme->pfn_end)) {
+				spin_unlock_irqrestore(&zone->lock, flags);
+				break; /* done with this rme */
+			}
+
+			adjust_zone_present_pages(zone, -(1 << order));
+
+			/* XXX: can we shrink spanned_pages & start_pfn without too much work? */
+
+			/* zone free stats */
+			zone->free_area[order].nr_free--;
+			__mod_zone_page_state(zone, NR_FREE_PAGES,
+					      -(1UL << order));
+
+			list_del(&page->lru);
+
+			spin_unlock_irqrestore(&zone->lock, flags);
+
+			/* Add page to new zone */
+			dnuma_prior_add_to_new_zone(page, order, dest_zone, range_nid);
+			return_pages_to_zone(page, order, dest_zone);
+			adjust_zone_present_pages(dest_zone, 1 << order);
+
+			continue;
+skip_unlock:
+			spin_unlock_irqrestore(&zone->lock, flags);
+		}
+	}
+}
