b6f255153740e1a6223f9d39ad7f1d36f7669b4a
Cody P Schafer <cody@linux.vnet.ibm.com>
2012-10-26 16:58:27 -0700

    x86/mm/numa: when dnuma is enabled, use memlayout to handle memory hotplug's physaddr_to_nid.
    When a memlayout is tracked (ie: CONFIG_DYNAMIC_NUMA is enabled), rather
    than iterate over numa_meminfo, a lookup can be done using memlayout.
    
    Signed-off-by: Cody P Schafer <cody@linux.vnet.ibm.com>

--- arch/x86/mm/numa.c
+++ arch/x86/mm/numa.c
@@ -28,7 +28,7 @@ struct pglist_data *node_data[MAX_NUMNODES] __read_mostly;
 EXPORT_SYMBOL(node_data);
 
 static struct numa_meminfo numa_meminfo
-#ifndef CONFIG_MEMORY_HOTPLUG
+#if !defined(CONFIG_MEMORY_HOTPLUG) || defined(CONFIG_DYNAMIC_NUMA)
 __initdata
 #endif
 ;
@@ -832,7 +832,7 @@ EXPORT_SYMBOL(cpumask_of_node);
 
 #endif	/* !CONFIG_DEBUG_PER_CPU_MAPS */
 
-#ifdef CONFIG_MEMORY_HOTPLUG
+#if defined(CONFIG_MEMORY_HOTPLUG) && !defined(CONFIG_DYNAMIC_NUMA)
 int memory_add_physaddr_to_nid(u64 start)
 {
 	struct numa_meminfo *mi = &numa_meminfo;
