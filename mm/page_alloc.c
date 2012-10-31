a49ca5b48b70570b1eb5b3ef5acdbe190d370e26
Cody P Schafer <cody@linux.vnet.ibm.com>
2012-10-31 14:18:43 -0700

    mm/page_alloc: bypass certain DEBUG_VM checks for dynamic numa.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -258,8 +258,10 @@ static int page_is_consistent(struct zone *zone, struct page *page)
 {
 	if (!pfn_valid_within(page_to_pfn(page)))
 		return 0;
+#ifndef CONFIG_DYNAMIC_NUMA
 	if (zone != page_zone(page))
 		return 0;
+#endif
 
 	return 1;
 }
