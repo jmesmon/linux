e5ced8d47148f2d4c3077d42423817b4661f4904
Cody P Schafer <cody@linux.vnet.ibm.com>
2012-11-13 16:21:16 -0800

    dnuma: grow spanned pages.

--- include/linux/dnuma.h
+++ include/linux/dnuma.h
@@ -40,13 +40,56 @@ static inline void dnuma_move_to_new_ml(struct memlayout *new_ml)
 	}
 }
 
+static inline struct zone *get_zone(int nid, enum zone_type zonenum)
+{
+	return &NODE_DATA(nid)->node_zones[zonenum];
+}
+
+/* Note: growth of spanned pages is not allowed if !defined(CONFIG_SPARSEMEM)
+ * due to pageblock_flags */
+static inline void dnuma_adjust_spanned_pages(unsigned long pfn,
+		struct zone *old_zone, struct pglist_data *old_node,
+		struct zone *new_zone, struct pglist_data *new_node)
+{
+	unsigned long flags, end_pfn;
+	pgdat_resize_lock(new_node, &flags);
+
+	/* grow new zone */
+	zone_span_writelock(new_zone);
+	end_pfn = new_zone->zone_start_pfn + new_zone->spanned_pages;
+	if (!new_zone->zone_start_pfn && !new_zone->spanned_pages) {
+		new_zone->zone_start_pfn = pfn;
+		new_zone->spanned_pages = 1;
+	} else if (pfn < new_zone->zone_start_pfn) {
+		new_zone->spanned_pages += new_zone->zone_start_pfn - pfn;
+		new_zone->zone_start_pfn = pfn;
+	} else if (pfn >= end_pfn) {
+		new_zone->spanned_pages  = pfn - new_zone->zone_start_pfn;
+	}
+	zone_span_writeunlock(new_zone);
+
+	/* grow new node */
+	end_pfn = new_node->node_start_pfn + new_node->node_spanned_pages;
+	if (!new_node->node_start_pfn && !new_node->node_spanned_pages) {
+		new_node->node_start_pfn = pfn;
+		new_node->node_spanned_pages = 1;
+	} else if (pfn < new_node->node_start_pfn) {
+		new_node->node_spanned_pages += new_node->node_start_pfn - pfn;
+		new_node->node_start_pfn = pfn;
+	} else if (pfn >= end_pfn) {
+		new_node->node_start_pfn = pfn - new_node->node_start_pfn;
+	}
+	pgdat_resize_unlock(new_node, &flags);
+}
+
 /* moves a page when it is being freed (and thus was not moved by
  * dnuma_move_free_pages()) */
 static inline struct zone *dnuma_move_free_page_zone(struct page *page)
 {
 	/* XXX: handle pageblock_flags */
 	enum zone_type zonenum = page_zonenum(page);
-	int new_nid = memlayout_pfn_to_nid_no_pageflags(page_to_pfn(page));
+	unsigned long pfn = page_to_pfn(page);
+	int new_nid = memlayout_pfn_to_nid_no_pageflags(pfn);
 	int old_nid = page_to_nid(page);
 
 	if (new_nid == NUMA_NO_NODE)
@@ -57,6 +100,9 @@ static inline struct zone *dnuma_move_free_page_zone(struct page *page)
 		/* XXX: fixup the old zone & new zone's present_pages & spanned_pages & zone_start_pfn */
 		/* use span_seqlock */
 		set_page_node(page, new_nid);
+
+		dnuma_adjust_spanned_pages(pfn, get_zone(old_nid, zonenum), NODE_DATA(old_nid),
+				get_zone(new_nid, zonenum), NODE_DATA(new_nid));
 
 		spin_lock_irqsave(&dnuma_stats_lock, flags);
 		if (dnuma_moved_page_ct < (~(u64)0))
