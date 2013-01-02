8df4b4f565c74a80e09c29b2c7880976d2d8a8a4
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-01-02 12:57:52 -0500

    mm/memory_hotplug: use {pgdat,zone}_is_empty() when resizing zones & pgdats
    Use the *_is_empty() helpers to be more clear about what we're actually
    checking for.
    
    Signed-off-by: Cody P Schafer <cody@linux.vnet.ibm.com>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -368,7 +368,7 @@ static void grow_pgdat_span(struct pglist_data *pgdat, unsigned long start_pfn,
 	unsigned long old_pgdat_end_pfn =
 		pgdat->node_start_pfn + pgdat->node_spanned_pages;
 
-	if (!pgdat->node_spanned_pages || start_pfn < pgdat->node_start_pfn)
+	if (pgdat_is_empty(pgdat) || start_pfn < pgdat->node_start_pfn)
 		pgdat->node_start_pfn = start_pfn;
 
 	pgdat->node_spanned_pages = max(old_pgdat_end_pfn, end_pfn) -
