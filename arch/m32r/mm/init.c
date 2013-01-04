d688f20374acd65329d017a40419df2acdd29804
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-01-04 14:33:54 -0500

    m32r/mm/init: use generic free_init_page_range()
    Switch to free_init_page_range() from open coding in freeing init mem &
    freeing the initrd mem.
    
    This results in nearly identical behavior with the addition of
    memset()ing the page range to POISON_FREE_INIT_MEM

--- arch/m32r/mm/init.c
+++ arch/m32r/mm/init.c
@@ -184,15 +184,9 @@ void __init mem_init(void)
  *======================================================================*/
 void free_initmem(void)
 {
-	unsigned long addr;
+	free_init_page_range((unsigned long)&__init_begin,
+			     (unsigned long)&__init_end);
 
-	addr = (unsigned long)(&__init_begin);
-	for (; addr < (unsigned long)(&__init_end); addr += PAGE_SIZE) {
-		ClearPageReserved(virt_to_page(addr));
-		init_page_count(virt_to_page(addr));
-		free_page(addr);
-		totalram_pages++;
-	}
 	printk (KERN_INFO "Freeing unused kernel memory: %dk freed\n", \
 	  (int)(&__init_end - &__init_begin) >> 10);
 }
@@ -204,13 +198,7 @@ void free_initmem(void)
  *======================================================================*/
 void free_initrd_mem(unsigned long start, unsigned long end)
 {
-	unsigned long p;
-	for (p = start; p < end; p += PAGE_SIZE) {
-		ClearPageReserved(virt_to_page(p));
-		init_page_count(virt_to_page(p));
-		free_page(p);
-		totalram_pages++;
-	}
+	free_init_page_range(start, end);
 	printk (KERN_INFO "Freeing initrd memory: %ldk freed\n", (end - start) >> 10);
 }
 #endif
