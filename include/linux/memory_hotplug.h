6411fbbc06ad8ff696d88214b3f1fbb3b00596c4
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-01-28 16:48:14 -0800

    memory_hotplug: factor out locks in mem_online_cpu()
    In dynamic numa, when onlining nodes, lock_memory_hotplug() is already
    held when mem_online_node()'s functionality is needed.
    
    Factor out the locking and create a new function __mem_online_node() to
    allow reuse.
    
    Signed-off-by: Cody P Schafer <cody@linux.vnet.ibm.com>

--- include/linux/memory_hotplug.h
+++ include/linux/memory_hotplug.h
@@ -248,6 +248,7 @@ static inline int is_mem_section_removable(unsigned long pfn,
 static inline void try_offline_node(int nid) {}
 #endif /* CONFIG_MEMORY_HOTREMOVE */
 
+extern int __mem_online_node(int nid);
 extern int mem_online_node(int nid);
 extern int add_memory(int nid, u64 start, u64 size);
 extern int arch_add_memory(int nid, u64 start, u64 size);
