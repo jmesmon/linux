76a5520abe902de5ab0a004b6ef5092503a54b13
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-02-19 15:06:37 -0800

    dnuma.c fixups.

--- mm/dnuma.c
+++ mm/dnuma.c
@@ -46,9 +46,7 @@ void dnuma_online_required_nodes_and_zones(struct memlayout *new_ml)
 		int nid = rme->nid;
 
 		if (!node_online(nid)) {
-			pr_debug("onlining node %d\n", nid);
-			/* Consult hotadd_new_pgdat() */
-			__mem_online_node(nid);
+			pr_info("onlining node %d [start]\n", nid);
 
 			/* XXX: somewhere in here do a memory online notify: we
 			 * aren't really onlining memory, but some code uses
@@ -82,6 +80,14 @@ void dnuma_online_required_nodes_and_zones(struct memlayout *new_ml)
 					memory_notify(MEM_CANCEL_ONLINE, &arg);
 				}
 			}
+
+			/* Consult hotadd_new_pgdat() */
+			__mem_online_node(nid);
+			if (!node_online(nid)) {
+				pr_alert("node %d not online after onlining\n", nid);
+			}
+
+			pr_info("onlining node %d [complete]\n", nid);
 		}
 
 		/* Determine the zones required */
@@ -148,16 +154,16 @@ static void dnuma_prior_return_to_new_zone(struct page *page, int order,
 	int i;
 	unsigned long pfn = page_to_pfn(page);
 
-	grow_pgdat_and_zone(dest_zone, pfn, pfn + (1 << order));
+	grow_pgdat_and_zone(dest_zone, pfn, pfn + (1UL << order));
 
-	for (i = 0; i < 1 << order; i++)
+	for (i = 0; i < 1UL << order; i++)
 		set_page_node(&page[i], dest_nid);
 }
 
 static void clear_lookup_node(struct page *page, int order)
 {
 	int i;
-	for (i = 0; i < 1 << order; i++)
+	for (i = 0; i < 1UL << order; i++)
 		ClearPageLookupNode(&page[i]);
 }
 
@@ -170,72 +176,127 @@ void dnuma_prior_free_to_new_zone(struct page *page, int order,
 	struct zone *curr_zone = page_zone(page);
 
 	/* XXX: Fiddle with 1st zone's locks */
-	adjust_zone_present_pages(curr_zone, -(1 << order));
+	adjust_zone_present_pages(curr_zone, -(1UL << order));
 
 	/* XXX: fiddles with 2nd zone's locks */
 	dnuma_prior_return_to_new_zone(page, order, dest_zone, dest_nid);
 }
 
-/* must be called with zone->lock held */
-static void remove_free_page_from_zone(struct zone *zone, struct page *page, int order)
+/* must be called with zone->lock held and memlayout's update_lock held */
+static void remove_free_pages_from_zone(struct zone *zone, struct page *page, int order)
 {
 	/* zone free stats */
 	zone->free_area[order].nr_free--;
-	__mod_zone_page_state(zone, NR_FREE_PAGES,
-			      -(1UL << order));
-	adjust_zone_present_pages(zone, -(1 << order));
+	__mod_zone_page_state(zone, NR_FREE_PAGES, -(1UL << order));
+	adjust_zone_present_pages(zone, -(1UL << order));
 
 	list_del(&page->lru);
-
-	/* avoid a VM_BUG in __free_page_ok */
-	VM_BUG_ON(!PageBuddy(page));
 	__ClearPageBuddy(page);
+
+	/* Allowed because we hold the memlayout update_lock. */
+	clear_lookup_node(page, order);
+
+	/* XXX: can we shrink spanned_pages & start_pfn without too much work?
+	 *  - not crutial because having a
+	 *    larger-than-necessary span simply means that more
+	 *    PFNs are iterated over.
+	 *  - would be nice to be able to do this to cut down
+	 *    on overhead caused by PFN iterators.
+	 */
 }
 
 /*
  * __ref is to allow (__meminit) zone_pcp_update(), which we will have because
- * DYNAMIC_NUMA depends on MEMORY_HOTPLUG.
+ * DYNAMIC_NUMA depends on MEMORY_HOTPLUG (and all the MEMORY_HOTPLUG comments
+ * indicate __meminit is allowed when they are enabled).
  */
-void __ref dnuma_move_unallocated_pages(struct memlayout *new_ml)
+static void __ref add_free_page_to_node(int dest_nid, struct page *page, int order)
 {
-	/* XXX: locking considerations:
-	 *  - what can cause the hotplugging of a node? Do we just need to
-	 *    lock_memory_hotplug()?
-	 */
+	bool need_zonelists_rebuild = false;
+	struct zone *dest_zone = nid_zone(dest_nid, page_zonenum(page));
+	VM_BUG_ON(!zone_is_initialized(dest_zone));
 
-	/* migrate types?
-	 * ISOLATION?
-	 */
+	if (zone_is_empty(dest_zone))
+		need_zonelists_rebuild = true;
+
+	/* Add page to new zone */
+	dnuma_prior_return_to_new_zone(page, order, dest_zone, dest_nid);
+	return_pages_to_zone(page, order, dest_zone);
+	dnuma_post_free_to_new_zone(page, order);
+
+	/* XXX: fixme, there are other states that need fixing up */
+	if (!node_state(dest_nid, N_MEMORY))
+		node_set_state(dest_nid, N_MEMORY);
+
+	if (need_zonelists_rebuild) {
+		/* XXX: also does stop_machine() */
+		//zone_pcp_reset(zone);
+		/* XXX: why is this locking actually needed? */
+		mutex_lock(&zonelists_mutex);
+		//build_all_zonelists(NULL, NULL);
+		build_all_zonelists(NULL, dest_zone);
+		mutex_unlock(&zonelists_mutex);
+	} else
+		/* FIXME: does stop_machine() after EVERY SINGLE PAGE */
+		/* XXX: this is probably wrong. What does "update" actually
+		 * indicate in zone_pcp terms? */
+		zone_pcp_update(dest_zone);
+}
+
+static struct rangemap_entry *add_split_pages_to_zones(
+		struct rangemap_entry *first_rme,
+		struct page *page, int order)
+{
+	int i;
+	struct rangemap_entry *rme = first_rme;
+	for (i = 0; i < (1 << order); i++) {
+		unsigned long pfn = page_to_pfn(page);
+		while (pfn > rme->pfn_end) {
+			rme = rme_next(rme);
+		}
+
+		add_free_page_to_node(rme->nid, page + i, 0);
+	}
+
+	return rme;
+}
+
+void dnuma_move_free_pages(struct memlayout *new_ml)
+{
+	/* FIXME: how does this removal of pages from a zone interact with
+	 * migrate types? ISOLATION? */
 	struct rangemap_entry *rme;
 	ml_for_each_range(new_ml, rme) {
-		int range_nid = rme->nid;
-		unsigned long pfn;
+		unsigned long pfn = rme->pfn_start;
+		int range_nid;
 		struct page *page;
+new_rme:
+		range_nid = rme->nid;
 
-		for (pfn = rme->pfn_start; pfn <= rme->pfn_end; pfn++) {
-			struct zone *zone, *dest_zone;
+		for (; pfn <= rme->pfn_end; pfn++) {
+			struct zone *zone;
 			int page_nid, order;
 			unsigned long flags, last_pfn, first_pfn;
-			bool need_zonelists_rebuild = false;
 			if (!pfn_valid(pfn))
 				continue;
 
 			page = pfn_to_page(pfn);
-
-			/* XXX: what can "unreserve" pages? */
 #if 0
+			/* XXX: can we ensure this is safe? Pages marked
+			 * reserved could be freed into the page allocator if
+			 * they mark memory areas that were allocated via
+			 * earlier allocators. */
 			if (PageReserved(page)) {
-				fiddle with the node;
-				adjust spanned_pages & present_pages & start_pfn.
+				set_page_node(page, range_nid);
+				/* TODO: adjust spanned_pages & present_pages & start_pfn. */
 			}
 #endif
 
 			/* Currently allocated, will be fixed up when freed. */
 			if (!PageBuddy(page))
-				goto skip_valid;
+				continue;
 
 			page_nid = page_to_nid(page);
-			/* Page is already in the "right" zone */
 			if (page_nid == range_nid)
 				continue;
 
@@ -247,67 +308,47 @@ void __ref dnuma_move_unallocated_pages(struct memlayout *new_ml)
 			if (!PageBuddy(page))
 				goto skip_unlock;
 
-			/* It has already migrated "somewhere" */
-			if (page_zone(page) != zone)
+			/* It has already been transplanted "somewhere",
+			 * somewhere should be the proper zone. */
+			if (page_zone(page) != zone) {
+				VM_BUG_ON(zone != nid_zone(range_nid, page_zonenum(page)));
 				goto skip_unlock;
+			}
 
-			dest_zone = nid_zone(range_nid, page_zonenum(page));
-			VM_BUG_ON(!zone_is_initialized(dest_zone));
-
-			if (zone_is_empty(dest_zone))
-				need_zonelists_rebuild = true;
-
-			/* FIXME: split pages when the range does not cover an
-			 * entire order */
 			order = page_order(page);
 			first_pfn = pfn & ~((1 << order) - 1);
 			last_pfn  = pfn |  ((1 << order) - 1);
-			if (WARN(pfn != first_pfn, "pfn %lu is not first_pfn %lu\n",
+			if (WARN(pfn != first_pfn, "pfn %05lx is not first_pfn %05lx\n",
 							pfn, first_pfn)) {
 				pfn = last_pfn;
 				goto skip_unlock;
 			}
 
-			if (WARN(last_pfn > rme->pfn_end,
-					"last_pfn %lu goes beyond end of rme [ %lu - %lu ]\n",
-					last_pfn, rme->pfn_start, rme->pfn_end)) {
+			if (last_pfn > rme->pfn_end) {
+				/* this higher order page doesn't fit into the
+				 * current range even though it starts there.
+				 */
+				pr_warn("high-order page from pfn %05lx to %05lx extends beyond end of rme {%05lx - %05lx}:%d\n",
+						first_pfn, last_pfn,
+						rme->pfn_start, rme->pfn_end,
+						rme->nid);
+
+				remove_free_pages_from_zone(zone, page, order);
 				spin_unlock_irqrestore(&zone->lock, flags);
-				break; /* done with this rme */
+
+				rme = add_split_pages_to_zones(rme, page, order);
+				pfn = last_pfn + 1;
+				goto new_rme;
 			}
 
-			clear_lookup_node(page, order);
-			remove_free_page_from_zone(zone, page, order);
-			/* XXX: can we shrink spanned_pages & start_pfn without too much work?
-			 *  - not crutial because having a
-			 *    larger-than-necessary span simply means that more
-			 *    PFNs are iterated over.
-			 *  - would be nice to be able to do this to cut down
-			 *    on overhead caused by PFN iterators.
-			 */
+			remove_free_pages_from_zone(zone, page, order);
 			spin_unlock_irqrestore(&zone->lock, flags);
 
-			/* Add page to new zone */
-			dnuma_prior_return_to_new_zone(page, order, dest_zone, range_nid);
-			return_pages_to_zone(page, order, dest_zone);
-			dnuma_post_free_to_new_zone(page, order);
-
-			/* XXX: fixme, there are other states that need fixing up */
-			if (!node_state(range_nid, N_MEMORY))
-				node_set_state(range_nid, N_MEMORY);
-
-			if (need_zonelists_rebuild)
-				/* XXX: also does stop_machine() */
-				build_all_zonelists(NULL, dest_zone);
-			else
-				/* FIXME: does stop_machine() after EVERY SINGLE PAGE */
-				zone_pcp_update(dest_zone);
-
+			add_free_page_to_node(range_nid, page, order);
 			pfn = last_pfn;
 			continue;
 skip_unlock:
 			spin_unlock_irqrestore(&zone->lock, flags);
-skip_valid:
-			;
 		}
 	}
 }
