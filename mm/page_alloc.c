7c098d8b2a4648c5058be21d03445c038d51a2d4
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-03-29 10:30:23 -0700

    mm: when handling percpu_pagelist_fraction, use on_each_cpu() to set percpu pageset fields.
    In free_hot_cold_page(), we rely on pcp->batch remaining stable.
    Updating it without being on the cpu owning the percpu pageset
    potentially destroys this stability.
    
    Change for_each_cpu() to on_each_cpu() to fix.
    
    Signed-off-by: Cody P Schafer <cody@linux.vnet.ibm.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5471,30 +5471,30 @@ int lowmem_reserve_ratio_sysctl_handler(ctl_table *table, int write,
 	return 0;
 }
 
+static void _zone_set_pageset_highmark(void *data)
+{
+	struct zone *zone = data;
+	unsigned long  high;
+	high = zone->managed_pages / percpu_pagelist_fraction;
+	setup_pagelist_highmark(per_cpu_ptr(zone->pageset, smp_processor_id()), high);
+}
+
 /*
  * percpu_pagelist_fraction - changes the pcp->high for each zone on each
  * cpu.  It is the fraction of total pages in each zone that a hot per cpu pagelist
  * can have before it gets flushed back to buddy allocator.
  */
-
 int percpu_pagelist_fraction_sysctl_handler(ctl_table *table, int write,
 	void __user *buffer, size_t *length, loff_t *ppos)
 {
 	struct zone *zone;
-	unsigned int cpu;
 	int ret;
 
 	ret = proc_dointvec_minmax(table, write, buffer, length, ppos);
 	if (!write || (ret < 0))
 		return ret;
-	for_each_populated_zone(zone) {
-		for_each_possible_cpu(cpu) {
-			unsigned long  high;
-			high = zone->managed_pages / percpu_pagelist_fraction;
-			setup_pagelist_highmark(
-				per_cpu_ptr(zone->pageset, cpu), high);
-		}
-	}
+	for_each_populated_zone(zone)
+		on_each_cpu(_zone_set_pageset_highmark, zone, true);
 	return 0;
 }
 
