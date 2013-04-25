96ac81a7410b5c001237cf345b92a80d95743cdd
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-04-25 12:14:25 -0700

    drivers/base/node: rename unregister_mem_blk_under_nodes() to be more acurate
    unregister_mem_block_under_nodes() only unregisters a single section in
    the mem block under all nodes, not the entire mem block. Rename it to
    unregister_mem_block_section_under_nodes(). Also rename the phys_index
    param to indicate that it is a section number.
    
    Signed-off-by: Cody P Schafer <cody@linux.vnet.ibm.com>

--- include/linux/node.h
+++ include/linux/node.h
@@ -38,8 +38,9 @@ extern int register_cpu_under_node(unsigned int cpu, unsigned int nid);
 extern int unregister_cpu_under_node(unsigned int cpu, unsigned int nid);
 extern int register_mem_block_under_node(struct memory_block *mem_blk,
 						int nid);
-extern int unregister_mem_block_under_nodes(struct memory_block *mem_blk,
-					   unsigned long phys_index);
+extern int unregister_mem_block_section_under_nodes(
+					struct memory_block *mem_blk,
+					unsigned long sec_nr);
 
 #ifdef CONFIG_HUGETLBFS
 extern void register_hugetlbfs_with_node(node_registration_func_t doregister,
@@ -67,8 +68,9 @@ static inline int register_mem_block_under_node(struct memory_block *mem_blk,
 {
 	return 0;
 }
-static inline int unregister_mem_block_under_nodes(struct memory_block *mem_blk,
-						  unsigned long phys_index)
+static inline int unregister_mem_block_section_under_nodes(
+						struct memory_block *mem_blk,
+						unsigned long sec_nr)
 {
 	return 0;
 }
