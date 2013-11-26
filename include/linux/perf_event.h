3d972907a5c3ea6aff51eba40f303eb5edb3eda5
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-11-25 16:59:26 -0800

    perf: add PMU_FORMAT_RANGE() helper for use by sw-like pmus
    Add PMU_FORMAT_RANGE() and PMU_FORMAT_RANGE_RESERVED() (for reserved
    areas) which generate functions to extract the relevent bits from
    event->attr.config{,1,2} for use by sw-like pmus where the
    'config{,1,2}' values don't map directly to hardware registers.
    
    Signed-off-by: Cody P Schafer <cody@linux.vnet.ibm.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -871,4 +871,21 @@ _name##_show(struct device *dev,					\
 									\
 static struct device_attribute format_attr_##_name = __ATTR_RO(_name)
 
+#define PMU_FORMAT_RANGE(name, attr_var, bit_start, bit_end)		\
+PMU_FORMAT_ATTR(name, #attr_var ":" #bit_start "-" #bit_end);		\
+PMU_FORMAT_RANGE_RESERVED(name, attr_var, bit_start, bit_end)
+
+#define PMU_FORMAT_RANGE_RESERVED(name, attr_var, bit_start, bit_end)	\
+static u64 event_get_##name##_max(void)					\
+{									\
+	int bits = (bit_end) - (bit_start) + 1;				\
+	return ((0x1ULL << (bits - 1ULL)) - 1ULL) |			\
+		(0xFULL << (bits - 4ULL));				\
+}									\
+static u64 event_get_##name(struct perf_event *event)			\
+{									\
+	return (event->attr.attr_var >> (bit_start)) &			\
+		event_get_##name##_max();				\
+}
+
 #endif /* _LINUX_PERF_EVENT_H */
