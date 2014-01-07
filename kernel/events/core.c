d2133c131607c14a3c4c8ea45817e98a32d41a56
Cody P Schafer <cody@linux.vnet.ibm.com>
2014-01-07 10:30:20 -0800

    perf: split the concept of PERF_GROUP_SOFTWARE in two
    Add PERF_GROUP_NO_TXN and PERF_GROUP_ALWAYS_SCHEDULABLE as components of
    PERF_GROUP_SOFTWARE. Also introduce an is_software_group() helper (which
    matches up with the is_software_event() check)
    
    Signed-off-by: Cody P Schafer <cody@linux.vnet.ibm.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1251,7 +1251,7 @@ static void perf_group_attach(struct perf_event *event)
 	if (group_leader == event)
 		return;
 
-	if (group_leader->group_flags & PERF_GROUP_SOFTWARE &&
+	if (is_software_group(group_leader) &&
 			!is_software_event(event))
 		group_leader->group_flags &= ~PERF_GROUP_SOFTWARE;
 
@@ -1785,7 +1785,7 @@ static int group_can_go_on(struct perf_event *event,
 	/*
 	 * Groups consisting entirely of software events can always go on.
 	 */
-	if (event->group_flags & PERF_GROUP_SOFTWARE)
+	if (event->group_flags & PERF_GROUP_ALWAYS_SCHEDULABLE)
 		return 1;
 	/*
 	 * If an exclusive group is already on, no other hardware
@@ -7068,7 +7068,7 @@ SYSCALL_DEFINE5(perf_event_open,
 			 */
 			pmu = group_leader->pmu;
 		} else if (is_software_event(group_leader) &&
-			   (group_leader->group_flags & PERF_GROUP_SOFTWARE)) {
+			   is_software_group(group_leader)) {
 			/*
 			 * In case the group is a pure software group, and we
 			 * try to add a hardware event, move the whole group to
